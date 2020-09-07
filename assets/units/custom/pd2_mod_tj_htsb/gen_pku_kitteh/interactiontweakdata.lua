698027fe2332fabf422bd1abf18941ad70000fac
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2020-09-07 10:46:21 +0100

    A21 - Kitteh
    - Added Kitteh loot as a additional thing to get for the meme vault loot + achievement.
    - Added DSOD achievement.
    - Begin working on a workaround for the laptops instances to stop showing contours until a player is close enough to interact with it.
    - Navigation tweaks

---
+++ assets/units/custom/pd2_mod_tj_htsb/gen_pku_kitteh/interactiontweakdata.lua
@@ -0,0 +1,12 @@
+Hooks:PostHook( InteractionTweakData, "init", "soosh_kitteh_loot_custom_interactions", function(self, tweak_data)
+
+	self.gen_pku_kitteh = {
+		text_id = "hud_int_hold_take_kitteh",
+		action_text_id = "hud_action_taking_kitteh",
+		timer = 5,
+		sound_start = "bar_bag_money",
+		sound_interupt = "bar_bag_money_cancel",
+		sound_done = "bar_bag_money_finished",
+		blocked_hint = "carry_block"
+	}
+end)
