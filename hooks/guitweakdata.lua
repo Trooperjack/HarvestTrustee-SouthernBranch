d00548ef0e4584fc518da9b2bda746229d53cabc
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2020-09-10 15:35:47 +0100

    A24 - PD2Maps Contact
    - Change heist contact to PD2Maps.

--- hooks/guitweakdata.lua
+++ hooks/guitweakdata.lua
@@ -1,10 +1,23 @@
-Hooks:PostHook( GuiTweakData, "init", "trooperjack_contact", function(self)
+Hooks:PostHook( GuiTweakData, "init", "tj_htsb_contacts", function(self)
 	local contact_data = {
 		id = "trooperjack",
 		name_id = "heist_contact_trooperjack",
 		{
 			desc_id = "heist_contact_trooperjack_desc",
 			video = "myh1",
+			post_event = nil
+		}
+		
+		id = "pd2concepts",
+		name_id = "heist_contact_pd2concepts",
+		{
+			desc_id = "heist_contact_pd2concepts_desc",
+			video = "dragon1",
+			post_event = nil
+		},
+		{
+			desc_id = "heist_contact_pd2concepts_crew_desc",
+			video = "dragon1",
 			post_event = nil
 		}
 	}
